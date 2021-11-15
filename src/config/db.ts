f9b158a841ab511889b07babc166091be903e28d
Wilder León <wilder_eduardo85@hotmail.com>
2021-11-14 21:39:07 -0300

    added mongoDB configuration

---
+++ src/config/db.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`
+
+const connectDB = async (): Promise<void> => {
+  const conn = await mongoose.connect(mongoURI)
+  console.log(`MongoDB connected: ${conn.connection.host}`)
+}
+
+export default connectDB
--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,12 @@ import express, { Application } from 'express'
 import dotenv from 'dotenv'
 import bootcampsRouter from './routes/bootcamps'
 import morgan from 'morgan'
+import connectDB from './config/db'
 
 dotenv.config()
 const app: Application = express()
+
+connectDB()
 
 // Dev loggin middleware
 if (process.env.NODE_ENV === 'development') {
@@ -15,6 +18,13 @@ app.use('/api/v1/bootcamps', bootcampsRouter)
 
 const port: number | string = process.env.PORT || 5000
 
-app.listen(port, () =>
+const server = app.listen(port, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${port}`)
 )
+
+// handle unhandled promise rejections
+process.on('unhandledRejection', (error: any, promise: any) => {
+  console.log(`Error: ${error.message}`)
+  // close server & exit process
+  server.close(() => process.exit(1))
+})
